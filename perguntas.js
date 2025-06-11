0b8ab90cfcb6515004f8761a78e1e44830c7530c
yastxr <yasmim.teixeira.nascimento@escola.pr.gov.br>
2025-06-11 14:30:27 +0000

    8485

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Programação',
+    'Quanto é 1 + 1?',
+    'È 2'
+)
+
+criaCartao(
+    'Geografia',
+    'QQual é o maior continente do mundo em termos de área?
+
+A) Áfrical a Françacapital da ?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
